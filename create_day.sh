b10ff919ac898ee5222f0e77b0bb14b7ffd55e24
mdestefano <madestefano@unisa.it>
2022-12-10 13:15:08 +0100

    Created add automation for script day files genration

---
+++ create_day.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+mkdir -p $SCRIPT_DIR/$1
+touch $SCRIPT_DIR/$1/input.txt
+touch $SCRIPT_DIR/$1/solution.py
+touch $SCRIPT_DIR/$1/test.py
